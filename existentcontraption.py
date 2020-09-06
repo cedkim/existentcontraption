c9ba8fe3f08f55d89111648018f2840b8ede6111
Cedric Kim <hello@cedric.today>
2020-09-06 14:52:24 +0000

    Initialize SVG document

--- existentcontraption.py
+++ existentcontraption.py
@@ -7,10 +7,15 @@ SVG files that can be used for cutting out
 little desk toys.
 """
 
+import svgwrite
+
 class ExistentContraption:
     def __init__(self, levelXml, scale=1):
         self.levelXml = levelXml
         self.scale = scale
+    def convertToSvg(self):
+        dwg = svgwrite.Drawing('existentcontraption.svg', size=(str(100 * self.scale) + 'mm', str(100 * self.scale) + 'mm'), profile='tiny')
+        return dwg.toString()
 
 if __name__ == '__main__':
     pass
