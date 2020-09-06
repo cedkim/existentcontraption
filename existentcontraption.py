dd6210d094ce9e6ba8b2455ce09138fef79306cd
Cedric Kim <hello@cedric.today>
2020-09-06 14:40:12 +0000

    Create initial existentcontraption.py file

---
+++ existentcontraption.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Existent Contraption Tool
+------
+Convert Fantastic Contraption level XML to
+SVG files that can be used for cutting out
+little desk toys.
+"""
+
+class ExistentContraption:
+    def __init__(self, levelXml):
+        self.levelXml = levelXml
+
+if __name__ == '__main__':
+    # TODO: create CLI interface
