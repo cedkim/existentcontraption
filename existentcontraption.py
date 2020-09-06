952d0a03f6970c81ccefefe6d3b5ee4d7065938b
Cedric Kim <hello@cedric.today>
2020-09-06 15:03:00 +0000

    Set up level XML parsing

--- existentcontraption.py
+++ existentcontraption.py
@@ -8,14 +8,20 @@ little desk toys.
 """
 
 import svgwrite
+import xml.etree.ElementTree as ET
 
 class ExistentContraption:
-    def __init__(self, levelXml, scale=1):
+    def __init__(self, levelXml='', scale=1):
         self.levelXml = levelXml
         self.scale = scale
     def convertToSvg(self):
-        dwg = svgwrite.Drawing('existentcontraption.svg', size=(str(100 * self.scale) + 'mm', str(100 * self.scale) + 'mm'), profile='tiny')
-        return dwg.toString()
+        if (len(self.levelXml) > 0):
+            xmlRoot = ET.fromstring(self.levelXml)
+            dwg = svgwrite.Drawing('existentcontraption.svg', size=(str(100 * self.scale) + 'mm', str(100 * self.scale) + 'mm'), profile='tiny')
+
+            return dwg.toString()
+        else:
+            return '<!-- Error: No level XML provided -->'
 
 if __name__ == '__main__':
     pass
