2594c098b558d3d3e3fa35d08ddfbb6cb2692394
Cedric Kim <hello@cedric.today>
2020-09-07 15:05:26 +0000

    Fix bugs and add joints

--- existentcontraption.py
+++ existentcontraption.py
@@ -52,14 +52,20 @@ class ExistentContraption:
                 if (child.tag == 'SolidRod') or (child.tag == 'HollowRod'): # check if block is a rod
                     for _ in range(2): # repeat adding rods two times to prevent model from collapsing
                         newDwg = svgwrite.drawing.Drawing(size=(str((int(child.find('width')) / (2 / self.scale)) + 2) + 'mm', str((10 * self.scale) + 2) + 'mm')) # create a drawing with the appropriate size
-                        newDwg.add(newDwg.rect(insert=(self.scale + 1, self.scale + 1), size=(int(child.find('width')) / (2 / self.scale), 10 * self.scale), stroke='black', stroke-width=self.scale)) # add the rod to the drawing
-                        newDwg.add(newDwg.circle(center=(8 * self.scale, 5 * self.scale), r=2.5*self.scale)) # add the first hinge slot
-                        newDwg.add(newDwg.circle(center=((int(child.find('width')) / (2 / self.scale)) - 8, 5 * self.scale), r=2.5*self.scale)) # add the second hinge slot
+                        newDwg.add(newDwg.rect(insert=(1, 1), size=(int(child.find('width')) / (2 / self.scale), 10 * self.scale), stroke='black', stroke-width=self.scale*0.8)) # add the rod to the drawing
+                        newDwg.add(newDwg.circle(center=(8 * self.scale, 5 * self.scale), r=2.5*self.scale, stroke='black', stroke-width=self.scale*0.8)) # add the first hinge slot
+                        newDwg.add(newDwg.rect(insert=(2 * self.scale, materialThickness / 2), size=(4 * self.scale, materialThickness), stroke='black', stroke-width=self.scale*0.8)) # add hole in first hinge slot
+                        newDwg.add(newDwg.circle(center=((int(child.find('width')) / (2 / self.scale)) - 8, 5 * self.scale), r=2.5*self.scale, stroke='black', stroke-width=self.scale*0.8)) # add the second hinge slot
+                        newDwg.add(newDwg.rect(insert=(int(child.find('width')) - (2 * self.scale), materialThickness / 2), size=(4 * self.scale, materialThickness), stroke='black', stroke-width=self.scale*0.8)) # add hole in second hinge spot
                         dwgList.append(newDwg) # add the drawing to the list of drawings
-                        thickness += 1
+                        thickness += 1 # increment the thickness (in layers)
+            for _ in range(thickness): # iterate through layers
+                newDwg = svgwrite.drawing.Drawing(size=(str((thickness * materialThickness) + 2) + 'mm', str(((4 * self.scale) - tolerance) + 2) + 'mm')) # create a drawing for the joint connector
+                newDwg.add(newDwg.rect(insert=(1, 1), size=(int(thickness * materialThickness), int((4 * self.scale) - tolerance), stroke='black', stroke-width=self.scale*0.8)) # add the joint connector to the drawing
+                dwgList.append(newDwg) # add the joint connector drawing to the list of drawings
             svgOutputs = [] # define a list for SVG string outputs
             for dwg in dwgList: # iterate over all drawings
-                svgOutputs.append(dwg.toString()) # convert the drawings to SVG strings and add them to the list
+                svgOutputs.append(dwg.toString()) # convert the drawings to SVG strings and add them to the output list
             return svgOutputs # return the output values
         else: # runs if design XML is an empty string
             return '<!-- Error: No design XML provided -->' # returns an error in case no XML is provided
