357df7ce5173892619d08796b35d737c4e287e5c
Cedric Kim <hello@cedric.today>
2020-09-07 16:12:54 +0000

    Implement CLI

--- existentcontraption.py
+++ existentcontraption.py
@@ -7,8 +7,10 @@ SVG files that can be used for cutting out
 little desk toys.
 """
 
+import sys
 import xml.etree.ElementTree as ET
 import svgwrite
+import fire
 
 class ECConstants:
     """Existent Contraption constant definition class
@@ -70,6 +72,35 @@ class ExistentContraption:
         else: # runs if design XML is an empty string
             return '<!-- Error: No design XML provided -->' # returns an error in case no XML is provided
 
+def convertFromXmlFile(path='',outputfolder='',materialthickness=5,scale=1,machine=''):
+    """
+    Returns sum of a and b
+    :param path: the path to your XML file
+    :param outputfolder: a folder to save outputs to
+    :param machine: which type of machine you're using ('laser' for laser cutter and 'fdm' for FDM 3D printer)
+    :param materialthickness: the thickness in mm of a sheet of material (default: 5)
+    :param scale: how many times larger than usual your output should be (default: 1)
+    :return: some info
+    """
+    toleranceToUse = 0
+    if machine == 'laser':
+        toleranceToUse = ECConstants.LASERTOLERANCE
+    elif machine == 'fdm':
+        toleranceToUse = ECConstants.FDMTOLERANCE
+    else:
+        return "Please specify a machine type ('laser' or 'fdm')"
+    openedFile = open(path, 'rt')
+    ec = ExistentContraption(designXml=openedFile.read(), scale=scale)
+    openedFile.close()
+    outputSvgData = ec.convertToSvg(tolerance=toleranceToUse, materialThickness=materialthickness)
+    if (outputfolder[-1] == "/") or (outputfolder[-1] == "\\"):
+        outputfolder = outputfolder[:-1]
+    sfIncrement = 0
+    for svgFile in outputSvgData:
+        sfIncrement += 1
+        f = open(outputfolder + "/" + str(sfIncrement) + ".svg", "wt")
+        f.write(svgFile)
+        f.close()
+
 if __name__ == '__main__':
-    """The main entrypoint for the EC CLI tool"""
-    pass # to-do
+    fire.Fire(convertFromXmlFile)
