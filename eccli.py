e00b241959b78231210d0f99fefc36dc1bd6d55a
Cedric Kim <hello@cedric.today>
2020-09-08 11:51:02 +0000

    Revert "Add SVG combine capabilities"
    This reverts commit c28c004b52933fffcb7eb1fa2f2eccf8d6ea67e5

--- eccli.py
+++ eccli.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 import sys
-import os
 from existentcontraption import *
-import ecmanufacture
 import fire
-def convertFromXmlFile(path='',outputfolder='',combine=True,materialthickness=5,scale=1,machine=''):
+def convertFromXmlFile(path='',outputfolder='',materialthickness=5,scale=1,machine=''):
     """
-    Converts an XML file to several SVG files
+    Returns sum of a and b
     :param path: the path to your XML file
     :param outputfolder: a folder to save outputs to
-    :param combine: whether or not to combine outputs into one SVG file (default: True)
     :param machine: which type of machine you're using ('laser' for laser cutter and 'fdm' for FDM 3D printer)
     :param materialthickness: the thickness in mm of a sheet of material (default: 5)
     :param scale: how many times larger than usual your output should be (default: 1)
@@ -34,13 +31,6 @@ def convertFromXmlFile(path='',outputfolder='',combine=True,materialthickness=5,
         f = open(outputfolder + "/" + str(sfIncrement) + ".svg", "wt")
         f.write(svgFile)
         f.close()
-    if combine:
-        files = []
-        for _ in range(sfIncrement)
-            files.append(outputfolder + "/" + str(sfIncrement + 1))
-        ecmanufacture.mergeFiles(filesToMerge=files,outputFilename=outputfolder + "/merged.svg")
-        for f in range(sfIncrement):
-            os.remove(outputfolder + "/" + str(sfIncrement + 1))
-    return 'SVG generation complete!'
+
 if __name__ == '__main__':
     fire.Fire(convertFromXmlFile)
